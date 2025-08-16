d7ea2d319a234b1dc30c7d49b1dbfcfc46f0ea1e
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-08-15 21:38:07 -0400

    backend_set_up_3: Deploy Python backend to Lambda

--- src/app.py
+++ src/app.py
@@ -1,11 +1,30 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": [
+    os.getenv("FRONTEND_AMPLIFY_URL"), 
+    os.getenv("FRONTEND_URL")
+]}})
+
+
 @app.route("/python")
 def home():
-    return jsonify({"message": "Hello from Flask Python on Lambda!"})
+    print("/python hit from frontend")
+    return {"frontend_url": os.getenv("FRONTEND_URL"),
+            "message": "Hello from /python that hit from frontend",
+            }
+
+#@app.route("/python")
+#def home():
+#    return jsonify({"message": "Hello from Flask Python on Lambda!"})
+
+
+if __name__ == "__main__":  
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
